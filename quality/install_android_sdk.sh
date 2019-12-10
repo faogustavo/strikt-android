bb427cfd84fe42ab4526579019d0038df0537951
Gustavo Fão <faogustavo@gmail.com>
2019-12-10 13:42:03 -0300

    removed shell scripts, trying another action

--- quality/install_android_sdk.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Quit when a command returns non-zero code
-# or a variable has not being set
-set -eu
-
-wget --quiet --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-sudo unzip -d $ANDROID_HOME android-sdk.zip > /dev/null

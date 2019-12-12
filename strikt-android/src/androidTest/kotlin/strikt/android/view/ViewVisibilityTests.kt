9c478cf16ea20d4962a47869a2555f14b3f0af4f
Gustavo Fão <faogustavo@gmail.com>
2019-12-12 13:03:07 -0300

    updated unit tests

---
+++ strikt-android/src/androidTest/kotlin/strikt/android/view/ViewVisibilityTests.kt
@@ -0,0 +1,115 @@
+package strikt.android.view
+
+import android.content.Context
+import android.view.View
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import strikt.android.BaseTestClass
+import strikt.api.expectCatching
+import strikt.api.expectThat
+import strikt.assertions.failed
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import strikt.assertions.message
+
+class ViewVisibilityTests : BaseTestClass() {
+
+    @Test
+    fun isVisible_withVisibleView_shouldSucceed() {
+        val subject = mockView(visibility = View.VISIBLE)
+
+        expectThat(subject).isVisible()
+    }
+
+    @Test
+    fun isInvisible_withInvisibleView_shouldSucceed() {
+        val subject = mockView(visibility = View.INVISIBLE)
+
+        expectThat(subject).isInvisible()
+    }
+
+    @Test
+    fun isGone_withGoneView_shouldSucceed() {
+        val subject = mockView(visibility = View.GONE)
+
+        expectThat(subject).isGone()
+    }
+
+    @Test
+    fun hasVisibility_withRightVisibility_shouldSucceed() {
+        val subject = mockView(visibility = View.VISIBLE)
+
+        expectThat(subject).hasVisibility(View.VISIBLE)
+    }
+
+    @Test
+    fun isVisible_withOtherVisibility_shouldFail() {
+        val subject = mockView(visibility = View.GONE)
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ is visible : found "GONE"""".trimMargin()
+
+        expectCatching { expectThat(subject).isVisible() }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun isInvisible_withOtherVisibility_shouldFail() {
+        val subject = mockView(visibility = View.VISIBLE)
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ is invisible : found "VISIBLE"""".trimMargin()
+
+        expectCatching { expectThat(subject).isInvisible() }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun isGone_withOtherVisibility_shouldFail() {
+        val subject = mockView(visibility = View.INVISIBLE)
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ is gone : found "INVISIBLE"""".trimMargin()
+
+        expectCatching { expectThat(subject).isGone() }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasVisibility_withWrongVisibility_shouldFail() {
+        val subject = mockView(visibility = View.INVISIBLE)
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ has visibility "VISIBLE" : found "INVISIBLE"""".trimMargin()
+
+        expectCatching { expectThat(subject).hasVisibility(View.VISIBLE) }
+            .failed()
+            .isA<java.lang.AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    private fun mockView(
+        visibility: Int = View.VISIBLE
+    ) = TestView(context).apply {
+        this.visibility = visibility
+    }
+
+    class TestView(context: Context) : View(context) {
+        override fun toString() = "TestView()"
+    }
+}
--- strikt-android/src/main/kotlin/strikt/android/ext/+ViewVisibilty.kt
+++ strikt-android/src/main/kotlin/strikt/android/ext/+ViewVisibilty.kt
@@ -3,8 +3,8 @@ package strikt.android.ext
 import android.view.View
 
 fun Int.toViewVisibility() = when(this) {
-    View.VISIBLE -> "Visible"
-    View.INVISIBLE -> "Invisible"
-    View.GONE -> "Gone"
+    View.VISIBLE -> "VISIBLE"
+    View.INVISIBLE -> "INVISIBLE"
+    View.GONE -> "GONE"
     else -> "Unknown"
 }

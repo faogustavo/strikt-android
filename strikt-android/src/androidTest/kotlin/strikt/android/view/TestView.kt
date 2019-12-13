911fae7400e31c6b66ce29decd1e0fc909bc7384
Gustavo Fão <faogustavo@gmail.com>
2019-12-13 19:47:37 -0300

    parent and child assertions

--- strikt-android/src/androidTest/kotlin/strikt/android/view/TestView.kt
+++ strikt-android/src/androidTest/kotlin/strikt/android/view/TestView.kt
@@ -2,7 +2,12 @@ package strikt.android.view
 
 import android.content.Context
 import android.view.View
+import android.widget.LinearLayout
 
-class TestView(context: Context) : View(context) {
-    override fun toString() = "TestView()"
+class TestContainer(context: Context, private val id: String = "") : LinearLayout(context) {
+    override fun toString() = "TestContainer($id)"
+}
+
+class TestView(context: Context, private val id: String = "") : View(context) {
+    override fun toString() = "TestView($id)"
 }
---
+++ strikt-android/src/androidTest/kotlin/strikt/android/view/ViewRelativesTests.kt
@@ -0,0 +1,116 @@
+package strikt.android.view
+
+import android.widget.TextView
+import org.junit.Before
+import org.junit.Test
+import strikt.android.BaseTestClass
+import strikt.api.expectCatching
+import strikt.api.expectThat
+import strikt.assertions.failed
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import strikt.assertions.message
+
+class ViewRelativesTests : BaseTestClass() {
+
+    private val parent = TestContainer(context, "parent")
+    private val parentWithoutChild = TestContainer(context, "parent-no-child")
+    private val child = TestView(context, "child")
+    private val viewWithoutParent = TestView(context, "no-parent")
+
+    @Before
+    fun setUp() {
+        parent.addView(child)
+    }
+
+    @Test
+    fun hasParent_withParent_shouldSucceed() {
+        expectThat(child).hasParent(parent)
+        expectThat(viewWithoutParent).not().hasParent(parent)
+    }
+
+    @Test
+    fun hasParent_withoutParent_shouldFail() {
+        val expectedMessage =
+            """▼ Expect that TestView(no-parent):
+            |  ✗ has parent TestContainer(parent)""".trimMargin()
+
+        expectCatching { expectThat(viewWithoutParent).hasParent(parent) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasChild_shouldSucceed() {
+        expectThat(parent).hasChild()
+    }
+
+    @Test
+    fun hasChild_withoutChild_shouldFail() {
+        val expectedMessage =
+            """▼ Expect that TestContainer(parent-no-child):
+            |  ✗ has a child""".trimMargin()
+
+        expectCatching { expectThat(parentWithoutChild).hasChild() }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasChildCount_shouldSucceed() {
+        expectThat(parent).hasChildCount(1)
+    }
+
+    @Test
+    fun hasChildCount_withWrongValue_shouldFail() {
+        val expectedMessage =
+            """▼ Expect that TestContainer(parent):
+            |  ✗ has 6 child : found 1""".trimMargin()
+
+        expectCatching { expectThat(parent).hasChildCount(6) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasChild_withView_shouldSucceed() {
+        expectThat(parent).hasChild(child)
+    }
+
+    @Test
+    fun hasChild_withOtherView_shouldFail() {
+        val expectedMessage =
+            """▼ Expect that TestContainer(parent):
+            |  ✗ has child TestView(no-parent)""".trimMargin()
+
+        expectCatching { expectThat(parent).hasChild(viewWithoutParent) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasChild_withType_shouldSucceed() {
+        expectThat(parent).hasChild(TestView::class.java)
+    }
+
+    @Test
+    fun hasChild_withoutType_shouldFail() {
+        val expectedMessage =
+            """▼ Expect that TestContainer(parent):
+            |  ✗ has child with type class android.widget.TextView""".trimMargin()
+
+        expectCatching { expectThat(parent).hasChild(TextView::class.java) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+}

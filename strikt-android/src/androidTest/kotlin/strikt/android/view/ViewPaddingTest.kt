3bbd71f7b762584890edc9b7c827e26649613c9b
Gustavo Fão <faogustavo@gmail.com>
2019-12-13 19:17:01 -0300

    padding assertions

---
+++ strikt-android/src/androidTest/kotlin/strikt/android/view/ViewPaddingTest.kt
@@ -0,0 +1,185 @@
+package strikt.android.view
+
+import org.junit.Test
+import strikt.android.BaseTestClass
+import strikt.api.expectCatching
+import strikt.api.expectThat
+import strikt.assertions.failed
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import strikt.assertions.message
+import java.lang.AssertionError
+
+class ViewPaddingTest : BaseTestClass() {
+
+    @Test
+    fun hasLeftPadding_withCorrectPadding_shouldSucceed() {
+        val expectedPadding = 16
+
+        val view = mockView(
+            left = expectedPadding
+        )
+
+        expectThat(view).hasLeftPadding(expectedPadding)
+    }
+
+    @Test
+    fun hasLeftPadding_withIncorrectPadding_shouldFail() {
+        val view = mockView(
+            left = 8
+        )
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ has left padding 16 : found 8""".trimMargin()
+        expectCatching { expectThat(view).hasLeftPadding(16) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasRightPadding_withCorrectPadding_shouldSucceed() {
+        val expectedPadding = 16
+
+        val view = mockView(
+            right = expectedPadding
+        )
+
+        expectThat(view).hasRightPadding(expectedPadding)
+    }
+
+    @Test
+    fun hasRightPadding_withIncorrectPadding_shouldFail() {
+        val view = mockView(
+            right = 8
+        )
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ has right padding 16 : found 8""".trimMargin()
+        expectCatching { expectThat(view).hasRightPadding(16) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasTopPadding_withCorrectPadding_shouldSucceed() {
+        val expectedPadding = 16
+
+        val view = mockView(
+            top = expectedPadding
+        )
+
+        expectThat(view).hasTopPadding(expectedPadding)
+    }
+
+    @Test
+    fun hasTopPadding_withIncorrectPadding_shouldFail() {
+        val view = mockView(
+            top = 8
+        )
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ has top padding 16 : found 8""".trimMargin()
+        expectCatching { expectThat(view).hasTopPadding(16) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasBottomPadding_withCorrectPadding_shouldSucceed() {
+        val expectedPadding = 16
+
+        val view = mockView(
+            bottom = expectedPadding
+        )
+
+        expectThat(view).hasBottomPadding(expectedPadding)
+    }
+
+    @Test
+    fun hasBottomPadding_withIncorrectPadding_shouldFail() {
+        val view = mockView(
+            bottom = 8
+        )
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ has bottom padding 16 : found 8""".trimMargin()
+        expectCatching { expectThat(view).hasBottomPadding(16) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasStartPadding_withCorrectPadding_shouldSucceed() {
+        val expectedPadding = 16
+
+        val view = mockView(
+            left = expectedPadding
+        )
+
+        expectThat(view).hasStartPadding(expectedPadding)
+    }
+
+    @Test
+    fun hasStartPadding_withIncorrectPadding_shouldFail() {
+        val view = mockView(
+            left = 8
+        )
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ has start padding 16 : found 8""".trimMargin()
+        expectCatching { expectThat(view).hasStartPadding(16) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun hasEndPadding_withCorrectPadding_shouldSucceed() {
+        val expectedPadding = 16
+
+        val view = mockView(
+            right = expectedPadding
+        )
+
+        expectThat(view).hasEndPadding(expectedPadding)
+    }
+
+    @Test
+    fun hasEndPadding_withIncorrectPadding_shouldFail() {
+        val view = mockView(
+            right = 8
+        )
+
+        val expectedMessage =
+            """▼ Expect that TestView():
+            |  ✗ has end padding 16 : found 8""".trimMargin()
+        expectCatching { expectThat(view).hasEndPadding(16) }
+            .failed()
+            .isA<AssertionError>()
+            .message
+            .isEqualTo(expectedMessage)
+    }
+
+    private fun mockView(
+        left: Int = 0,
+        top: Int = 0,
+        right: Int = 0,
+        bottom: Int = 0
+    ) = TestView(context).apply {
+        setPadding(left, top, right, bottom)
+    }
+}

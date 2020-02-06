a9f7d28b456b8feed41b37dd62e7b9e3bd5923d2
Bernat Borrás <bernat.borras@schibsted.com>
2020-02-06 08:24:00 +0100

    Add test isUri

---
+++ strikt-android/src/androidTest/kotlin/strikt/android/UriTests.kt
@@ -0,0 +1,25 @@
+package strikt.android
+
+import android.net.Uri
+import org.junit.Test
+import strikt.api.expectThat
+
+class UriTests {
+
+    @Test
+    fun stringIsUri_succeed() {
+        val uri = mockUri().toString()
+
+        expectThat(uri).isUri()
+    }
+
+    private fun mockUri(): Uri = Uri.EMPTY.buildUpon()
+        .scheme("https")
+        .authority("banana.net")
+        .appendPath("coconunt")
+        .appendPath("kiwi")
+        .appendQueryParameter("size", "big")
+        .appendQueryParameter("spice", "pepper")
+        .build()
+
+}
